e0a0f28c072036738973fa748b5a3a631427dc4c
cedric <cedric@loulou.home>
2017-04-30 20:42:46 +0200

    added the LayerMesh to the map

--- src/scgame.cpp
+++ src/scgame.cpp
@@ -14,6 +14,8 @@ void Game::setup ()
 {
   DEBUG("game setup");
   this->map.setup();
+  iso::LayerMesh layer(256, 256);
+  this->map.add_layer(&layer);
   this->display = al_create_display(640, 400);
   al_rest(2);
 }
